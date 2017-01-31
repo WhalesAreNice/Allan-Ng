7737e8d3e3f999a7d97bd3d15948756082d525c3
WhalesAreNice <an1994xp@gmail.com>
2017-01-31 17:46:13 -0500

    Add files via upload

---
+++ MMP 210/week 1/css/javascript exercise/js/sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(800,800);
+    line(15, 25, 70, 90);
+    ellipse(300, 400, 600, 700);
+    ellipse(175, 350, 150, 75);
+    ellipse(425, 350, 150, 75);
+    
+}
+
